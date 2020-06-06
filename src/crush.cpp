c57f3ad42c2933c472b899508352568da9e5c126
Peter "Rocky" Villano <peter.s.villano@gmail.com>
2020-06-06 02:04:54 -0400

    dropped an O(n)

--- src/crush.cpp
+++ src/crush.cpp
@@ -8,29 +8,21 @@
 using namespace std;
 
 long arrayManipulation(int n, const vector<vector<int>> &queries) {
-    map<int, long> sparse_arr;
-    sparse_arr[0] = 0;
-    sparse_arr[n - 1] = 0;
+    //O(n log n) version
+    vector<pair<int, long>> fuck(queries.size() * 2);
     for (const auto &query : queries) {
         const int start = query[0] - 1;
         const int end = query[1];
-        sparse_arr[start] = 0;
-        sparse_arr[end] = 0;
+        const int val = query[2];
+        fuck.emplace_back(start, val);
+        fuck.emplace_back(end, -val);
     }
-    for (const auto &query : queries) {
-        const int start = query[0] - 1;
-        const int end = query[1];
-        const int fill = query[2];
-        for (auto it = sparse_arr.find(start); it != sparse_arr.find(end); ++it) {
-            it->second += fill;
-        }
+    sort(fuck.begin(), fuck.end());
+    long running = 0;
+    long best = 0;
+    for (const auto &it : fuck) {
+        running += it.second;
+        best = max(best, running);
     }
-    auto pr = max_element(
-            begin(sparse_arr),
-            end(sparse_arr),
-            [](const auto &p1, const auto &p2) {
-                return p1.second < p2.second;
-            }
-    );
-    return pr->second;
+    return best;
 }
